70a1fadc438a0ca96891a6bedd6612f392be8180
ZenScript12 <zentest12131@gmail.com>
2024-11-20 01:14:37 +0700

    Prime Hub Main Loader.lua

--- Prime Hub Main Loader.lua
+++ Prime Hub Main Loader.lua
@@ -1,16 +1,37 @@
-## Hi there 👋
+local a = 0
+local Place = game.PlaceId
+local GameName = game:GetService("MarketplaceService"):GetProductInfo(Place).Name
+local baseURL = "https://raw.githubusercontent.com/meobeo8/elgato/a/"
+local placeScripts = {
+    [78681874345659] = "ErrorZone.lua",
+    [86639052909924] = "VersePiece.lua",
+    [89438510123061] = "AnimeShadow.lua",
+    [92998083931728] = "VersePieceRaid.lua"
+}
+local scriptName = placeScripts[Place]
 
-<!--
-**ZenScript12/ZenScript12** is a ✨ _special_ ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+if scriptName then
+    loadstring(game:HttpGet(baseURL .. scriptName))()
+    a = a + 1
+    if a > 2 then
+        game.Players.LocalPlayer:Kick("Executed too many times")
+    end
+else
+    game.Players.LocalPlayer:Kick("This game is not supported or has been removed")
+end
 
-Here are some ideas to get you started:
+spawn(function()
+    while wait(1) do
+        pcall(function()
+            for _, v in pairs(game:GetService("Lighting"):GetChildren()) do
+                if v.Name == "Blur" then
+                    v:Destroy()
+                end
+            end
+        end)
+    end
+end)
 
-- 🔭 I’m currently working on ...
-- 🌱 I’m currently learning ...
-- 👯 I’m looking to collaborate on ...
-- 🤔 I’m looking for help with ...
-- 💬 Ask me about ...
-- 📫 How to reach me: ...
-- 😄 Pronouns: ...
-- ⚡ Fun fact: ...
--->
+for _, v in next, getconnections(game:GetService("Players").LocalPlayer.Idled) do
+    v:Disable()
+end
